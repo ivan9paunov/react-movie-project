b5190d5a053530bef092155c3a556b34585ab77a
ivan9paunov <73014962+ivan9paunov@users.noreply.github.com>
2021-06-05 03:34:42 +0300

    Add files via upload

--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,7 @@ const constants = {
     baseUrl: 'https://api.themoviedb.org/3',
     apiKey: 'f1f5877c7d8c646b47bf279e9130fdd1',
     posterPath: 'https://www.themoviedb.org/t/p/w220_and_h330_face/',
+    addMoviePoster: 'https://res.cloudinary.com/ivanpaunov/image/upload/v1622036509/movie-covers/',
+    addTvSeriePoster: 'https://res.cloudinary.com/ivanpaunov/image/upload/v1622302177/tvserie-covers/',
 }
 export default constants;
